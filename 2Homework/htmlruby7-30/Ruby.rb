ed923585f99bd72852640bf4c9516e0bdd8a8c5e
Belinda Hare <apple@apples-MacBook-Pro.local>
2014-07-31 00:43:02 -0400

    July 30 Homework
    Includes view folder with 3 erb files, 1 css file, 3 photo files, and
    one ruby file.

---
+++ 2Homework/htmlruby7-30/Ruby.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'Faker'
+
+@Faker = Faker::Internet.email
+# homepage
+get '/' do
+  erb :Home, layout: :siteformat
+end
+# book 1?
+get '/1' do
+  erb :Book1, layout: :siteformat
+end
+
+get '/2' do
+  erb :Book2, layout: :siteformat
+end
+
