fcff0010dcfc123d267f6a15ba1a12562586d09c
Belinda Hare <apple@apples-MacBook-Pro.local>
2014-08-05 20:53:02 -0400

    Hospital Database
    first commit

---
+++ 3Homework/Hospital_Aug5/app/controllers/patients_controller.rb
@@ -0,0 +1,37 @@
+class PatientsController < ApplicationController
+
+  def show
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.create patient_params
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    @patient = Patient.find params [:id]
+    @patient.update_attributes patient_params
+    redirect_to root_path 
+  end
+
+  def destroy
+    redirect_to root_path
+  end
+
+  private 
+    def patient_params
+      params.require(:patient).permit(:first_name, :last_name, :dob, :description, :gender)
+    end
+    def find_patient
+      @patient = Patient.find params[:id]
+    end
+end
+
+
---
+++ 3Homework/Hospital_Aug5/app/models/patient.rb
@@ -0,0 +1,19 @@
+class Patient < ActiveRecord::Base
+  def show
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update 
+  end
+  def destroy
+  end
+  private 
+    def food_params
+    end
+    def find_patient
+    end
+end
